9f91e20a3df21996ebf16673a25a2834fc3d3567
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2023-03-11 19:40:04 +0200

    Calendar.jsava: prepared: minimal

--- NativeModuleProject/android/app/src/main/java/com/nativemoduleproject/CalendarModule.java
+++ NativeModuleProject/android/app/src/main/java/com/nativemoduleproject/CalendarModule.java
@@ -1,4 +1,24 @@
 package com.nativemoduleproject;
 
-public class CalendarModule {
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import java.util.Map;
+import java.util.HashMap;
+
+
+
+public class CalendarModule extends ReactContextBaseJavaModule {
+    CalendarModule(ReactApplicationContext context) {
+        super(context);
+    }
+
+    // add to CalendarModule.java
+    @Override
+    public String getName() {
+        return "CalendarModule";
+    }
+
 }
